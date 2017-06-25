8eb53fa129d1c4d5e1aaf843a3321dc6221c1ceb
Benellos <fdmhtc@gmail.com>
2017-06-25 18:52:25 +0200

    Bugfix

--- newsBot/app/config/properties.js
+++ newsBot/app/config/properties.js
@@ -1,6 +1,6 @@
 //properties.js
 /** Facebook */
-exports.facebook_token = EAAbZA6M9RPGgBAMFlIjTZCcswlypq0DQKiB6fkWYHXr8syON0fQtvHMnR4etvscuFm4271VZAnUzDXk4HYPfIiz18fJYe4TpfvWy3HJpiTH1smUSiyxEPiQ89whqaCJOZBqKKcKVJ1mCw6S45bNZBBUGtPiZAzg1QSLYRc9jaqrAZDZD;
+exports.facebook_token = "EAAbZA6M9RPGgBAMFlIjTZCcswlypq0DQKiB6fkWYHXr8syON0fQtvHMnR4etvscuFm4271VZAnUzDXk4HYPfIiz18fJYe4TpfvWy3HJpiTH1smUSiyxEPiQ89whqaCJOZBqKKcKVJ1mCw6S45bNZBBUGtPiZAzg1QSLYRc9jaqrAZDZD";
 exports.facebook_challenge = 'hello_token_success';
 exports.facebook_message_endpoint = 'https://graph.facebook.com/v2.6/me/messages';
 
